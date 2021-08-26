d353dffc40f15878bd970714bc68e91b0b445d1f
Nicola Hastings <nicolahastings@gmail.com>
2021-08-26 15:13:00 -0400

    NH | ad space | featured event | what we do | nursing jobs/journals CTA tiles

--- cna-general.js
+++ cna-general.js
@@ -24,8 +24,41 @@ function handleLanguageButton() {
     $('.language-btn').prependTo('#MPheader > div.row:first-child > .col-md-12');
 }
 
+function handleFlexContentItems() {
+    $('.flex-content-item').each(function () {
+        var self = $(this).find('.HtmlContent');
+
+        $(self).find('> img').wrap('<div class="img-container" />');
+        $(self).find('> *:not(.img-container)').wrapAll('<div class="text-container" />');
+    });
+}
+
+function handleCTAButtons() {
+    $('.cta-button').wrapAll('<div class="cta-buttons" />');
+}
+
+function handleCTATiles() {
+    $('.cta-tile .HtmlContent').each(function () {
+        var self = $(this),
+            link = $(self).find('a'),
+            href = $(link).attr('href'),
+            target = $(link).attr('target');
+
+        if (target == '_blank') {
+            $(self).wrapInner('<a href="' + href + '" target="_blank" rel="noopener" />');
+        } else {
+            $(self).wrapInner('<a href="' + href + '" />');
+        }
+
+        $(link).hide();
+    });
+}
+
 $(function () {
     handleHeaderLinks();
     handleSearch();
     handleLanguageButton();
+    handleFlexContentItems();
+    handleCTAButtons();
+    handleCTATiles();
 });
