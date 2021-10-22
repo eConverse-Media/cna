5f0cce9fae6d415237dc7beb560f85786a5366af
Nicola Hastings <nicolahastings@gmail.com>
2021-10-22 17:10:49 -0400

    NH | Community landing page

--- cna-community-page.js
+++ cna-community-page.js
@@ -3,4 +3,5 @@ $(function () {
     $('#PageTitleH1').wrap('<div class="community-page-heading" />');
     $('.summary-edit').appendTo('.community-page-heading');
     $('.JoinLeaveLink').appendTo('.community-page-heading');
+    $('#CommunityTabsContainer').prependTo('.community-page-heading');
 });
