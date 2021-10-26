011b4bad2bd324976e2d26117c4e63dce229a27a
Nicola Hastings <nicolahastings@gmail.com>
2021-10-26 17:04:14 -0400

    NH | FR updates

--- cna-general-fr.js
+++ cna-general-fr.js
@@ -2,6 +2,15 @@ function updateSearch() {
     $('.search-bar-top .form-control').attr('placeholder', 'Rechercher...');
 }
 
+function updateFeaturedNews() {
+    $('.featured-news .Content ul li').each(function () {
+        var self = $(this);
+
+        $(self).find('.text-container h4').text('Apprendre plus');
+    });
+}
+
 $(function () {
     updateSearch();
+    updateFeaturedNews();
 });
